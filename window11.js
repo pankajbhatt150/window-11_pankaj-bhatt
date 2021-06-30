54b4600259e407ca76d12bdbd91bcc47a9fd1ec5
pankajbhatt150 <76560049+pankajbhatt150@users.noreply.github.com>
2021-06-30 13:33:42 +0530

    Add files via upload

---
+++ window11.js
@@ -0,0 +1,12 @@
+var taskbar = document.getElementsByClassName("taskbar")[0]
+var startmenu = document.getElementsByClassName("startmenu")[0]
+
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px"
+    }
+})
